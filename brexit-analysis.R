c183e797679b9b3abc781de24631b6442958672c
Nik Silver <nik@niksilver.com>
2019-04-04 20:22:36 +0100

    Extracted the motions, votes and some other metadata

--- brexit-analysis.R
+++ brexit-analysis.R
@@ -15,3 +15,35 @@ for ( url in csv.urls ) {
   csv = read.csv( url )
   raw.data = rbind( raw.data, csv )
 }
+
+vote.uri.to.name <- function(uri) {
+  if (uri == "http://data.parliament.uk/schema/parl#AyeVote" ) {
+    return( "Aye" )
+  } else if (uri == "http://data.parliament.uk/schema/parl#NoVote" ) {
+    return( "No" )
+  } else {
+    return( "???" )
+  }
+}
+
+motion.id.and.vote.title <- function( division.number, title, vote.name ) {
+  vote.title = paste( title, " (", vote.name, ")", sep = "" )
+  if (vote.name == "Aye") {
+    return( c( division.number * 10 + 1, vote.title ))
+  } else if (vote.name == "No") {
+    return( c( division.number * 10 + 0, vote.title ))
+  } else {
+    return( c( division.number * 10 + 9, vote.title ))
+  }
+}
+
+motions = unique( raw.data[ c(  "division.number", "vote...type...uri", "title" )])
+motions$vote.name = lapply( motions$vote...type...uri, vote.uri.to.name)
+motions[ c("id", "vote.title")]  =
+  t(
+    mapply( motion.id.and.vote.title
+          , motions$division.number
+          , motions$title
+          , motions$vote.name
+          )
+  )
