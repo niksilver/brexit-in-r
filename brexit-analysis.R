22cbeb2031d0633fd14a46cfd94deed8e88a12a1
Nik Silver <nik@niksilver.com>
2019-04-07 15:26:44 +0100

    Now doing all the right calculations and outputting node and edge files

--- brexit-analysis.R
+++ brexit-analysis.R
@@ -1,8 +1,10 @@
 # Convert the "pretty" URLs, which presents a nice page of data, into the URLs for CSV files.
 
 pretty.urls =
-  c( "http://explore.data.parliament.uk/?endpoint=commonsdivisions/id/1041567"
-     ,"http://explore.data.parliament.uk/?endpoint=commonsdivisions/id/1041564"
+  c( "http://explore.data.parliament.uk/?endpoint=commonsdivisions/id/1041567"  # PM's MV 1
+     , "http://explore.data.parliament.uk/?endpoint=commonsdivisions/id/1041564"  # Baron (f)
+     #, "http://explore.data.parliament.uk/?endpoint=commonsdivisions/id/1042258"  # No confidence
+     #, "http://explore.data.parliament.uk/?endpoint=commonsdivisions/id/1050712"  # Brady(n)
   )
 url.ids = sub( ".*/", "", pretty.urls )
 csv.urls = paste( "http://lda.data.parliament.uk/commonsdivisions/id/", url.ids, ".csv", sep = "" )
@@ -24,6 +26,7 @@ print("Making calculations...")
 # - vote name which is just the string Aye or No (reduced from the URI)
 # - vote title to indicate not just the title of the motion, but also whether it's Aye or No version
 # - vote id to give an id to the vote title
+# - count of votes in that lobby for that motion
 
 vote.uri.to.name <- function(uri) {
   if (uri == "http://data.parliament.uk/schema/parl#AyeVote" ) {
@@ -31,7 +34,7 @@ vote.uri.to.name <- function(uri) {
   } else if (uri == "http://data.parliament.uk/schema/parl#NoVote" ) {
     return( "No" )
   } else {
-    return( "???" )
+    stop( "Unknown vote URI" )
   }
 }
 
@@ -45,7 +48,7 @@ get.vote.id <- function( division.number, vote.name ) {
   } else if (vote.name == "No") {
     return( division.number * 10 + 0 )
   } else {
-    return( division.number * 10 + 9 )
+    stop( "Unknown vote name" )
   }
 }
 
@@ -61,16 +64,34 @@ v.get.vote.title = Vectorize(get.vote.title)
 
 raw.data$vote.title = v.get.vote.title( raw.data$title, raw.data$vote.name )
 
+get.vote.count = function(ayes, noes, vote.name) {
+  if (vote.name == "Aye") {
+    ayes
+  } else if (vote.name == "No") {
+    noes
+  } else {
+    stop("Got a vote which is neither Aye nor No")
+  }
+}
+
+v.get.vote.count = Vectorize(get.vote.count)
+
+raw.data$vote.count = v.get.vote.count(
+    raw.data$ayes.count, raw.data$noesvotecount, raw.data$vote.name
+  )
+
+
 # Get a data frame with just the motions and the vote for each
 
 voted.motions = unique( data.frame(
   id = raw.data$vote.id
   , title = raw.data$vote.title
   , name = raw.data$vote.name
+  , count = raw.data$vote.count
   ))
 
 # Get combinations of all pairs of voted motions.
-# This has columns id.x, title.x, name.x, id.y, title.y, name.y
+# This has columns id.x, title.x, name.x, count.x ... and the same for .y
 
 cross.votes = merge(voted.motions, voted.motions, by = NULL)
 
@@ -102,3 +123,28 @@ v.get.count.both = Vectorize(get.count.both)
 cross.votes$count.both = v.get.count.both( cross.votes$id.x, cross.votes$id.y )
 
 cross.votes$affinity = cross.votes$count.both / cross.votes$count.either
+
+# Output the nodes and edges for Gephi
+
+votes.nodes = data.frame(
+  Id = voted.motions$id
+  , Label = voted.motions$title
+  , VoteName = voted.motions$name
+  , Count = voted.motions$count
+)
+
+filtered.votes.edges =
+  cross.votes[ cross.votes$id.x < cross.votes$id.y & cross.votes$count.both > 0, ]
+
+votes.edges = data.frame(
+  Source = filtered.votes.edges$id.x
+  , Target = filtered.votes.edges$id.y
+  , Weight = filtered.votes.edges$affinity
+  , CountEither = filtered.votes.edges$count.either
+  , CountBoth = filtered.votes.edges$count.both
+)
+
+print("Writing files...")
+
+write.csv(votes.nodes, file = "~/../Desktop/votes-nodes.csv", row.names = FALSE)
+write.csv(votes.edges, file = "~/../Desktop/votes-edges.csv", row.names = FALSE)
